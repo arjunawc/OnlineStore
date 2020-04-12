a555b0909d343e2e172436060230afa2905a8fb7
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-12 21:09:34 +1200

    Implement search functionality

--- OnlineStore.Core/Specifications/ProductSpecParams.cs
+++ OnlineStore.Core/Specifications/ProductSpecParams.cs
@@ -4,6 +4,7 @@
     {
         private const int MaxPageSize = 50;
         private int _pageSize = 6;
+        private string _search;
 
         public int PageIndex { get; set; } = 1;        
 
@@ -16,5 +17,11 @@
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
         public string Sort { get; set; }
+        
+        public string Search 
+        {
+            get => _search;
+            set => _search = value.ToLower();
+        }
     }
 }
--- OnlineStore.Core/Specifications/ProductWithFiltersForCountSpecification.cs
+++ OnlineStore.Core/Specifications/ProductWithFiltersForCountSpecification.cs
@@ -1,7 +1,4 @@
 ﻿using OnlineStore.Core.Entities;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace OnlineStore.Core.Specifications
 {
@@ -9,6 +6,8 @@ namespace OnlineStore.Core.Specifications
     {
         public ProductWithFiltersForCountSpecification(ProductSpecParams productParams)
             : base(x =>
+                (string.IsNullOrEmpty(productParams.Search) || x.Name.ToLower()
+                    .Contains(productParams.Search)) && 
                 (!productParams.BrandId.HasValue || x.ProductBrandId == productParams.BrandId) &&
                 (!productParams.TypeId.HasValue || x.ProductTypeId == productParams.TypeId)
             )
