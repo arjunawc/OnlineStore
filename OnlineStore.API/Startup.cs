74d5a291a1e1ebbe9d67dcad10269dc51afdf272
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-13 00:43:02 +1200

    Add CORS support to the API

--- OnlineStore.API/Startup.cs
+++ OnlineStore.API/Startup.cs
@@ -30,6 +30,13 @@ namespace OnlineStore.API
 
             services.AddApplicationServices();
             services.AddSwaggerDocumentation();
+            services.AddCors(opt =>
+            {
+                opt.AddPolicy("CorsPolicy", policy =>
+                {
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,6 +51,8 @@ namespace OnlineStore.API
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
