ca1a7c6aaf068e36a088476e36a808f3f2e8c8d9
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-12 16:17:09 +1200

    Clean up the Startup class by adding Service Extensions

--- OnlineStore.API/Startup.cs
+++ OnlineStore.API/Startup.cs
@@ -1,17 +1,13 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi.Models;
-using OnlineStore.API.Errors;
+using OnlineStore.API.Extensions;
 using OnlineStore.API.Helpers;
 using OnlineStore.API.Middleware;
-using OnlineStore.Core.Interfaces;
 using OnlineStore.Infrastructure.Data;
-using System.Linq;
 
 namespace OnlineStore.API
 {
@@ -26,37 +22,14 @@ namespace OnlineStore.API
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        {           
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlServer(_config.GetConnectionString("DefaultConnection")));
 
-            /* Configure [ApiController] behavior for Invalid model state validation */
-            services.Configure<ApiBehaviorOptions>(options =>
-            {
-                options.InvalidModelStateResponseFactory = actionContext =>
-                {
-                    var errors = actionContext.ModelState
-                        .Where(e => e.Value.Errors.Count > 0)
-                        .SelectMany(x => x.Value.Errors)
-                        .Select(x => x.ErrorMessage).ToArray();
-
-                    var errorResponse = new ApiValidationErrorResponse
-                    {
-                        Errors = errors
-                    };
-
-                    return new BadRequestObjectResult(errorResponse);
-                };
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "OnlineStore API", Version = "v1" });
-            });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -74,8 +47,7 @@ namespace OnlineStore.API
 
             app.UseAuthorization();
 
-            app.UseSwagger();
-            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "OnlineStore API v1"); });
+            app.UseSwaggerDocumentation();
 
             app.UseEndpoints(endpoints =>
             {
