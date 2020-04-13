8f3bea2914fd990a50cc16dca85bfeea02fb4750
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-13 12:30:11 +1200

    Remove unnecessary files

--- OnlineStore.API/Extensions/ApplicationServicesExtensions.cs
+++ OnlineStore.API/Extensions/ApplicationServicesExtensions.cs
@@ -11,7 +11,6 @@ namespace OnlineStore.API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
             /* Configure [ApiController] behavior for Invalid model state validation */
