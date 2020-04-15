b73cdd5ce48f478c85848902210948aef3a876d8
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-15 15:56:13 +1200

    Implement Unit of Work pattern

--- OnlineStore.API/Extensions/ApplicationServicesExtensions.cs
+++ OnlineStore.API/Extensions/ApplicationServicesExtensions.cs
@@ -14,6 +14,7 @@ namespace OnlineStore.API.Extensions
         {
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IOrderService, OrderService>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddScoped<IBasketRepository, BasketRepository>();
             services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
---
+++ OnlineStore.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using OnlineStore.Core.Entities;
+using System;
+using System.Threading.Tasks;
+
+namespace OnlineStore.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        IGenericRepository<TEntity> Repository<TEntity>() where TEntity : BaseEntity;
+        Task<int> Complete();
+    }
+}
