0b0d58266589cb1bab3cd1bb309277a8ae9c84d9
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-15 16:04:34 +1200

    Update the Generic Repository

--- OnlineStore.Core/Interfaces/IGenericRepository.cs
+++ OnlineStore.Core/Interfaces/IGenericRepository.cs
@@ -12,5 +12,8 @@ namespace OnlineStore.Core.Interfaces
         Task<T> GetOneAsync(ISpecification<T> spec);
         Task<IReadOnlyList<T>> ListAsync(ISpecification<T> spec);
         Task<int> CountAsync(ISpecification<T> spec);
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
     }
 }
