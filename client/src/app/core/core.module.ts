1c0a2af9ae9b0664463e84d046e822e57ec3c976
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-26 02:44:49 +1200

    install toastr and show error messages

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -4,12 +4,17 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ServerErrorComponent } from './server-error/server-error.component';
+import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
   declarations: [NavBarComponent, NotFoundComponent, ServerErrorComponent],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-bottom-right',
+      preventDuplicates: true
+    })
   ],
   exports: [NavBarComponent],
 })
