56bd71fde381d15e8ed9d427dae0f96524b1f1a8
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-26 01:59:59 +1200

    Error handling: Use the HttpInterceptor to catch errors

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { ShopComponent } from './shop/shop.component';
-import { ProductDetailsComponent } from './shop/product-details/product-details.component';
+import { ServerErrorComponent } from './core/server-error/server-error.component';
+import { NotFoundComponent } from './core/not-found/not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'server-error', component: ServerErrorComponent },
+  { path: 'not-found', component: NotFoundComponent },
   { path: 'shop', loadChildren: () => import('./shop/shop.module').then(mod => mod.ShopModule) },
   { path: '**', redirectTo: '', pathMatch: 'full' } // bad url redirect to home
 ];
