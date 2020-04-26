b2367f1816909d730ec9f84a76af53ab6a692e11
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-27 01:34:51 +1200

    Persisting the basket on startup

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,10 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
-import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
-import { NgxSpinnerModule } from 'ngx-spinner'
+import { NgxSpinnerModule } from 'ngx-spinner';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
 
 @NgModule({
--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -20,6 +20,7 @@ export class BasketService {
     return this.http.get(this.baseUrl + 'basket?id=' + id).pipe(
       map((basket: IBasket) => {
         this.basketSource.next(basket);
+        console.log(this.getCurrentBasketValue());
       })
     );
   }
