1fbaa861562013f7da4854f2dea492041ae1f913
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-26 20:08:19 +1200

    Install and setup ngx-spinner service

---
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -0,0 +1,19 @@
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { BusyService } from '../services/busy.service';
+import { finalize } from 'rxjs/operators';
+
+@Injectable()
+export class LoadingInterceptor implements HttpInterceptor {
+    constructor(private busyService: BusyService) {}
+
+    intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        this.busyService.busy();
+        return next.handle(req).pipe(
+            finalize(() => {
+                this.busyService.idle();
+            })
+        );
+    }
+}
