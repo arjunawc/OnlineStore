d0385eda73664c4d72deb5335370b9d05c1ade87
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-27 14:57:22 +1200

    add checkout module

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -12,6 +12,8 @@ const routes: Routes = [
   data: { breadcrumb: 'Shop' } },
   { path: 'basket', loadChildren: () => import('./basket/basket.module').then(mod => mod.BasketModule),
   data: { breadcrumb: 'Basket' } },
+  { path: 'checkout', loadChildren: () => import('./checkout/checkout.module').then(mod => mod.CheckoutModule),
+  data: { breadcrumb: 'Checkout' } },
   { path: '**', redirectTo: 'not-found', pathMatch: 'full' } // bad url redirect to not found
 ];
 
