8ec743844da4dd8dab0dd0a1d9d980142a4c67eb
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-28 12:21:27 +1200

    Create register form and dealing with modelstate errors

--- client/src/app/account/register/register.component.ts
+++ client/src/app/account/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AccountService } from '../account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,7 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-  constructor() {}
+  registerForm: FormGroup;
+  errors: string[];
 
-  ngOnInit(): void {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private accountService: AccountService,
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.createRegisterForm();
+  }
+
+  createRegisterForm() {
+    this.registerForm = this.formBuilder.group({
+      displayName: [null, [Validators.required]],
+      email: [
+        null,
+        [
+          Validators.required,
+          Validators.pattern('^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'),
+        ],
+      ],
+      password: [null, [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    this.accountService.register(this.registerForm.value).subscribe(
+      (response) => {
+        this.router.navigateByUrl('/shop');
+      },
+      (error) => {
+        this.errors = error.errors;
+        console.log(error);
+      }
+    );
+  }
 }
