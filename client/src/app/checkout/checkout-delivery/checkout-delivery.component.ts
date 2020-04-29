caff620a3e482f1538a714bc7f18efcc56da6d5f
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-29 19:20:06 +1200

    Update the shipping price

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { CheckoutService } from '../checkout.service';
 import { IDeliveryMethod } from 'src/app/shared/models/deliveryMethod';
 import { FormGroup } from '@angular/forms';
+import { BasketService } from 'src/app/basket/basket.service';
 
 @Component({
   selector: 'app-checkout-delivery',
@@ -12,7 +13,10 @@ export class CheckoutDeliveryComponent implements OnInit {
   @Input() checkoutForm: FormGroup;
   deliveryMethods: IDeliveryMethod[];
 
-  constructor(private checkoutService: CheckoutService) {}
+  constructor(
+    private checkoutService: CheckoutService,
+    private basketService: BasketService
+  ) {}
 
   ngOnInit(): void {
     this.checkoutService.getDeliveryMethods().subscribe(
@@ -23,5 +27,9 @@ export class CheckoutDeliveryComponent implements OnInit {
         console.log(error);
       }
     );
+  }
+
+  setShippingPrice(deliveryMethod: IDeliveryMethod) {
+    this.basketService.setShippingPrice(deliveryMethod);
   }
 }
