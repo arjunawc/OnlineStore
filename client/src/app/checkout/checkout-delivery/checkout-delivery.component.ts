6ecf1f846485b1a65b21562afaf7c2c6dcd01420
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-29 13:53:46 +1200

    Add the Delivery step component

--- client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
+++ client/src/app/checkout/checkout-delivery/checkout-delivery.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { CheckoutService } from '../checkout.service';
+import { IDeliveryMethod } from 'src/app/shared/models/deliveryMethod';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout-delivery',
   templateUrl: './checkout-delivery.component.html',
-  styleUrls: ['./checkout-delivery.component.scss']
+  styleUrls: ['./checkout-delivery.component.scss'],
 })
 export class CheckoutDeliveryComponent implements OnInit {
+  @Input() checkoutForm: FormGroup;
+  deliveryMethods: IDeliveryMethod[];
 
-  constructor() { }
+  constructor(private checkoutService: CheckoutService) {}
 
   ngOnInit(): void {
+    this.checkoutService.getDeliveryMethods().subscribe(
+      (dms: IDeliveryMethod[]) => {
+        this.deliveryMethods = dms;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
-
 }
--- client/src/app/checkout/checkout.service.ts
+++ client/src/app/checkout/checkout.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { IDeliveryMethod } from '../shared/models/deliveryMethod';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CheckoutService {
-  constructor() {}
+  baseUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {}
+
+  getDeliveryMethods() {
+    return this.http.get(this.baseUrl + 'orders/deliveryMethods').pipe(
+      map((deliveryMethods: IDeliveryMethod[]) => {
+        return deliveryMethods.sort((a, b) => b.price - a.price);
+      })
+    );
+  }
 }
