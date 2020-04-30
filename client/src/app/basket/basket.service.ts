9f7957fbc258aa1773481dffb9a73bee5a3250b7
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-30 15:45:53 +1200

    Create payment intent when navigate from review to payment

--- client/src/app/basket/basket.service.ts
+++ client/src/app/basket/basket.service.ts
@@ -20,6 +20,16 @@ export class BasketService {
 
   constructor(private http: HttpClient) {}
 
+  createPaymentIntent() {
+    return this.http.post(this.baseUrl + 'payments/' + this.getCurrentBasketValue().id, {})
+    .pipe(
+      map((basket: IBasket) => {
+        this.basketSource.next(basket);
+        console.log( this.getCurrentBasketValue());
+      })
+    );
+  }
+
   setShippingPrice(deliveryMethod: IDeliveryMethod) {
     this.shipping = deliveryMethod.price;
     const basket = this.getCurrentBasketValue();
--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -2,18 +2,34 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IBasket } from 'src/app/shared/models/basket';
 import { BasketService } from 'src/app/basket/basket.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-checkout-review',
   templateUrl: './checkout-review.component.html',
-  styleUrls: ['./checkout-review.component.scss']
+  styleUrls: ['./checkout-review.component.scss'],
 })
 export class CheckoutReviewComponent implements OnInit {
   basket$: Observable<IBasket>;
 
-  constructor(private basketService: BasketService) {}
+  constructor(
+    private basketService: BasketService,
+    private toastr: ToastrService
+  ) {}
 
   ngOnInit(): void {
     this.basket$ = this.basketService.basket$;
+  }
+
+  cretePaymentIntent() {
+    this.basketService.createPaymentIntent().subscribe(
+      (response: any) => {
+        this.toastr.success('Payment intent created');
+      },
+      (error) => {
+        console.log(error);
+        this.toastr.error(error.message);
+      }
+    );
   }
 }
