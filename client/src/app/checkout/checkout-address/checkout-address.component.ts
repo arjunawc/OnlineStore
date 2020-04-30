5b938ef1bef63d43afa62231ff90516473f9d550
Arjuna Sumathiratne <arjunawc@hotmail.com>
2020-04-30 22:49:41 +1200

    Enabling linear mode on stepper and disable access from header text, diable address save if already saved

--- client/src/app/checkout/checkout-address/checkout-address.component.ts
+++ client/src/app/checkout/checkout-address/checkout-address.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { AccountService } from 'src/app/account/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { IAddress } from 'src/app/shared/models/address';
 
 @Component({
   selector: 'app-checkout-address',
@@ -22,8 +23,9 @@ export class CheckoutAddressComponent implements OnInit {
     this.accountService
       .updateUserAddress(this.checkoutForm.get('addressForm').value)
       .subscribe(
-        () => {
+        (address: IAddress) => {
           this.toastr.success('Address saved');
+          this.checkoutForm.get('addressForm').reset(address);
         },
         (error) => {
           this.toastr.error(error.message);
--- client/src/app/checkout/checkout-review/checkout-review.component.ts
+++ client/src/app/checkout/checkout-review/checkout-review.component.ts
@@ -26,12 +26,10 @@ export class CheckoutReviewComponent implements OnInit {
   cretePaymentIntent() {
     this.basketService.createPaymentIntent().subscribe(
       (response: any) => {
-        this.toastr.success('Payment intent created');
         this.appStepper.next();
       },
       (error) => {
         console.log(error);
-        this.toastr.error(error.message);
       }
     );
   }
